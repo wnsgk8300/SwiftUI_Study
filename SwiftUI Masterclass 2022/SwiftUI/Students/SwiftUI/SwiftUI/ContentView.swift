1c4da6bd01faf4c9bf255eca99391c70fffc45b4
Junha <wnsgk830@gmail.com>
2022-07-11 09:49:41 +0900

    project

---
+++ SwiftUI Masterclass 2022/SwiftUI/Students/SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by photypeta-junha on 2022/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Masterclass 2022/SwiftUI/Students/SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by photypeta-junha on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
