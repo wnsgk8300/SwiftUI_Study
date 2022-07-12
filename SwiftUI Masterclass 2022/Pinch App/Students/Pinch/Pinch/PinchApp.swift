55d8b23566346a11e2ea4d972d7875413b9bd9a2
Junha <wnsgk830@gmail.com>
2022-07-12 16:59:38 +0900

    Create Pinch project

---
+++ SwiftUI Masterclass 2022/Pinch App/Students/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by photypeta-junha on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
